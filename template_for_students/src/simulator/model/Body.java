cfa36d42f7751a659c7b6199d2aefddbe8c0719f
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-03-04 23:50:24 +0100

    Creación de la clase Body con algunos métodos y sus atributos.

--- template_for_students/src/simulator/model/Body.java
+++ template_for_students/src/simulator/model/Body.java
@@ -1,4 +1,45 @@
 package simulator.model;
 
-public class Body {
+import simulator.misc.Vector2D;
+
+public class Body { //Representa una entidad física
+	protected String identif;
+	protected Vector2D velocity;
+	protected Vector2D force;
+	protected Vector2D position;
+	protected double mass;
+	
+	
+	/*Getters y setters*/
+	
+	public String  getId() {
+		return identif;
+	}
+	public Vector2D getVelocity() {
+		return velocity;
+	}
+	public Vector2D getForce() {
+		return force;
+	}
+	public Vector2D getPosition() {
+		return position;
+	}
+	public double getMass() {
+		return mass;
+	}
+	
+	/*Métodos*/
+	
+	void addForce(Vector2D f) {
+		force = force.plus(f);
+	}
+	
+	void resetForce() {
+		force = force.scale(0);
+	}
+	
+	void move(double t) {
+		
+	}
+	
 }
