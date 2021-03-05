65fcca908f8c9c456a7cc020b712a6803465001e
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-03-05 09:17:34 +0100

    Creación de la calse BuilderBasedFactory

---
+++ template_for_students/src/simulator/factories/BuilderBasedFactory.java
@@ -0,0 +1,19 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+import java.util.List;
+
+public class BuilderBasedFactory implements Factory{
+		
+	BuilderBasedFactory(List<Builder<T>> list){
+		
+	}
+	
+	public  T createInstance(JSONObject json){
+		
+	}
+	
+	public List<JSONObject>getInfo(){
+		
+	}
+}
