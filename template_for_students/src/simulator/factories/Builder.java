fd078d2cbfda128cd7652651161ee67b4395ddb0
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-03-05 09:35:47 +0100

    Creación de nuevas clases en el paquete de factorías y en la clase Body(método getState())

---
+++ template_for_students/src/simulator/factories/Builder.java
@@ -0,0 +1,18 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+
+public abstract class Builder<T> {
+	
+	
+	
+	/*Metodos*/
+	
+	public T createInstance(JSONObject info) {
+		
+	}
+	
+	public JSONObject getBuilderInfo() {
+		
+	}
+}
