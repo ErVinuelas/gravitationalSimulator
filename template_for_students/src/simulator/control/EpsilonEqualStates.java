6c2d47054b456ee1fce417c151b97ece6bc576a1
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-03-08 09:58:13 +0100

    Cambios en la clase Epsilon Compare y actualización del Readme

--- template_for_students/src/simulator/control/EpsilonEqualStates.java
+++ template_for_students/src/simulator/control/EpsilonEqualStates.java
@@ -1,5 +1,6 @@
 package simulator.control;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import simulator.misc.Vector2D;
 
@@ -16,7 +17,9 @@ public class EpsilonEqualStates {
 		return Math.abs(a-b) <= eps;
 	}
 	
-	private boolean equals(Vector2D v1, Vector2D v2) {
+	private boolean equals(JSONArray jV1, JSONArray jV2) {
+		Vector2D v1 = new Vector2D(jV1.getInt(0), jV1.getInt(1));
+		Vector2D v2 = new Vector2D(jV2.getInt(0), jV2.getInt(1));
 		return v1.distanceTo(v2) <= eps;
 	}
 	
@@ -24,8 +27,23 @@ public class EpsilonEqualStates {
 		
 		if(s1.getDouble("time") != s2.getDouble("time")) {	return false;	}
 		
+		JSONArray jBodies1 = s1.getJSONArray("bodies");
+		JSONArray jBodies2 = s2.getJSONArray("bodies");	
 		
+		int length = jBodies1.length();
+				
+		if(length != jBodies2.length()) {return false;	}
 		
+		for(int i = 0; i < length; ++i) {
+			JSONObject bd1 = jBodies1.getJSONObject(i);
+			JSONObject bd2 = jBodies2.getJSONObject(i);
+			if(!bd1.getString("id").equals(bd2.getString("id"))) {	return false;	} //Comparamos identificadores
+			if(!this.equals(bd1.getJSONObject("data").getDouble("m"),bd2.getJSONObject("data").getDouble("m"))) {	return false;	}//Comparamos masas
+			if(!this.equals(bd1.getJSONObject("data").getJSONArray("p"),bd2.getJSONObject("data").getJSONArray("p"))) {	return false;	}//Comparamos posiciones
+			if(!this.equals(bd1.getJSONObject("data").getJSONArray("v"),bd2.getJSONObject("data").getJSONArray("v"))) {	return false;	}//Comparamos velocidades
+			if(!this.equals(bd1.getJSONObject("data").getJSONArray("f"),bd2.getJSONObject("data").getJSONArray("f"))) {	return false;	}//Comparamos fuerzas
+		}
+
 		return true;
 	}
 
