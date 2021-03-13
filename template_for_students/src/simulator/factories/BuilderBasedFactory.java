ba3008b5f0c974bbda0685313502a06f2d676721
Javi153 <jjimen06@ucm.es>
2021-03-13 12:47:05 +0100

    BuilderBasedFactory hecha, solo resta main y controller

--- template_for_students/src/simulator/factories/BuilderBasedFactory.java
+++ template_for_students/src/simulator/factories/BuilderBasedFactory.java
@@ -1,23 +1,36 @@
 package simulator.factories;
 
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class BuilderBasedFactory<T> implements Factory<T>{
+	
+	private List<Builder<T>> builders;
 		
 	BuilderBasedFactory(List<Builder<T>> list){
-		
+		builders = list;
 	}
 
 	@Override
-	public T createInstance(JSONObject info) {
-		// TODO Auto-generated method stub
-		return null;
+	public T createInstance(JSONObject info) throws IllegalArgumentException{
+		T inst = null;
+		for(Builder<T> b : builders) {
+			inst = b.createInstance(info);
+			if(inst != null) {
+				return inst;
+			}
+		}
+		throw new IllegalArgumentException();
 	}
 
 	@Override
 	public List<JSONObject> getInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		List<JSONObject> listjo = new ArrayList<>();
+		for(Builder<T> b : builders) {
+			listjo.add(b.getBuilderInfo());
+		}
+		return listjo;
 	}
 }
