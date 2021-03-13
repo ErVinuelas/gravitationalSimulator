b9df8075de0a82f4569aefbf290989c35af82aa7
ErVinuelas <pablo.martin.vinuelas@gmail.com>
2021-03-13 19:20:48 +0100

    En este commit he terminado la clase Controller y añadido cambios en la clase Physics Simulator

--- template_for_students/src/simulator/control/Controller.java
+++ template_for_students/src/simulator/control/Controller.java
@@ -4,8 +4,11 @@ import simulator.model.PhysicsSimulator;
 import simulator.model.Body;
 import simulator.factories.Factory;
 import java.io.InputStream;
+import java.io.OutputStream;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.json.JSONArray;
+import java.io.PrintStream;
 
 public class Controller {
 	
@@ -20,11 +23,44 @@ public class Controller {
 	}
 	
 	public void loadBodies(InputStream in) {
+
 		JSONObject jsonInput = new JSONObject(new JSONTokener(in));
+
 		int length = jsonInput.getJSONArray("bodies").length();
-		for(int i = 0; i < length; ++i) {
+		for (int i = 0; i < length; ++i) {
 			JSONObject jBd = jsonInput.getJSONArray("bodies").getJSONObject(i);
-			
+			Body newBody = constructor.createInstance(jBd);
+			simulator.addBody(newBody);
 		}
 	}
+	
+	public void run(int n, OutputStream out, InputStream expOut, StateComparator cmp) throws NonEqualStatesException {
+		JSONArray jStates = null;
+		if (expOut != null) {
+			JSONObject jsonInput = new JSONObject(new JSONTokener(expOut));
+			jStates = jsonInput.getJSONArray("states");
+		}
+
+		PrintStream p = new PrintStream(out);
+		p.println("{");
+		p.println("\"states\": [");
+
+		JSONObject s0 = simulator.getState();
+		p.println(s0);
+
+		for (int i = 0; i < n; ++i) {
+			simulator.advance();
+			JSONObject aux = simulator.getState();
+			if (expOut != null) {
+				if (!cmp.equal(aux, jStates.getJSONObject(i))) {
+					throw new NonEqualStatesException(aux, jStates.getJSONObject(i), n);
+				}
+			}
+			p.println(aux);
+		}
+
+		p.println("]");
+		p.println("}");
+	}
+	
 }
--- template_for_students/src/simulator/model/PhysicsSimulator.java
+++ template_for_students/src/simulator/model/PhysicsSimulator.java
@@ -1,60 +1,60 @@
 package simulator.model;
 
-
 import java.util.ArrayList;
 import org.json.*;
 
 public class PhysicsSimulator {
-	
+
 	private double dt;
 	private ArrayList<ForceLaws> laws;
 	private ArrayList<Body> bodies;
-	
+
 	private double time = 0.0;
-	
-	/*Constructor*/
-	
+
+	/* Constructor */
+
 	public PhysicsSimulator(double dt, ArrayList<ForceLaws> laws) {
 		this.dt = dt;
 		this.laws = laws;
 		bodies = new ArrayList<Body>();
 	}
-	
-	
-	/*Métodos*/
-	
+
+	/* Métodos */
+
 	public void advance() {
-		for(Body bd : bodies)
+		for (Body bd : bodies)
 			bd.resetForce();
-		for(ForceLaws law : laws)
+		for (ForceLaws law : laws)
 			law.apply(bodies);
-		for(Body bd : bodies)
+		for (Body bd : bodies)
 			bd.move(dt);
-		time += dt;			
+		time += dt;
 	}
-	
+
 	public void addBody(Body b) {
-		for(Body bd : bodies)
-			if(bd.equals(b)) {	throw new IllegalArgumentException();	}
+		for (Body bd : bodies)
+			if (bd.equals(b)) {
+				throw new IllegalArgumentException();
+			}
 		bodies.add(b);
 	}
-	
+
 	public JSONObject getState() {
 		JSONObject simulator = new JSONObject();
 		simulator.append("time", time);
 		ArrayList<JSONObject> jBodies = new ArrayList<JSONObject>();
-		for(Body bd : bodies) {
+		for (Body bd : bodies) {
 			JSONObject jBody = new JSONObject();
 			jBody = bd.getState();
 			jBodies.add(jBody);
 		}
 		simulator.append("bodies", jBodies);
-		
+
 		return simulator;
 	}
-	
+
 	public String toString() {
 		return getState().toString();
 	}
-	
+
 }
